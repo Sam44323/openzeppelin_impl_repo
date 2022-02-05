23d21afd9604742cd927423fbc7d6bea99aa8ca8
Suranjan <suranjanbiswas2002@gmail.com>
2022-02-05 15:46:38 +0530

    add: added initial test setup for NFT_token

--- test/unit/NftToken.ts
+++ test/unit/NftToken.ts
@@ -1,5 +1,21 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { Contract } from "ethers";
 import { ethers } from "hardhat";
 
 describe("Testing the NFT Token", () => {
-  let NftToken: Contract, owner;
+  let NftToken: Contract,
+    owner: SignerWithAddress,
+    recipient: SignerWithAddress;
+
+  beforeEach(async () => {
+    [owner, recipient] = await ethers.getSigners();
+    NftToken = await (await ethers.getContractFactory("NftToken"))
+      .connect(owner)
+      .deploy();
+  });
+
+  it("Checking the owner of the NFT_contract", async () => {
+    expect(await NftToken.owner()).to.equal(owner.address);
+  });
 });
