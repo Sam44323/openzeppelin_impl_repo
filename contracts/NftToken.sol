80abbbe44883d1baf6e110948f5cf93e0f6bab8f
Suranjan <suranjanbiswas2002@gmail.com>
2022-02-05 15:10:42 +0530

    add: added contract for minting an NFT

---
+++ contracts/NftToken.sol
@@ -0,0 +1,26 @@
+//contracts/NftToken.sol
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NftToken is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("NftToken", "NFT") {}
+
+    function mint(address recipient, string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 currentId = _tokenIds.current();
+        _mint(recipient, currentId);
+        _setTokenURI(currentId, tokenURI);
+
+        return currentId;
+    }
+}
